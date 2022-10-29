1cae0cf443a98ac8cb24c5d978e31579d2ae6a9d
KvRae <karamelmannai@gmail.com>
2022-10-29 23:47:08 +0100

    working with login form #1

--- app/src/main/java/devsec/app/easykitchen/activities/LoginActivity.kt
+++ app/src/main/java/devsec/app/easykitchen/activities/LoginActivity.kt
@@ -6,12 +6,17 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 import devsec.app.easykitchen.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val usernameLayout = findViewById<TextInputLayout>(R.id.LoginInputLayout)
+        val passwordLayout = findViewById<TextInputLayout>(R.id.PasswordInputLayout)
+        val usernameEditText = findViewById<TextView>(R.id.loginEditText)
+        val passwordEditText = findViewById<TextView>(R.id.passwordEditText)
 
         val registerBtn = findViewById<TextView>(R.id.RegisterTV)
 
@@ -23,11 +28,12 @@ class LoginActivity : AppCompatActivity() {
 
         val loginBtn = findViewById<Button>(R.id.LoginBtn)
         loginBtn.setOnClickListener() {
-            if (true) {
+            if (validateLogin(usernameEditText, passwordEditText,passwordLayout)) {
                 val intent = Intent(this, MainMenuActivity::class.java)
+                intent.putExtra("username", usernameEditText.text.toString())
                 startActivity(intent)
             }else{
-                val toast = Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG)
+                val toast = Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT)
                 toast.show()
             }
 
@@ -36,4 +42,29 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+    private fun validateLogin(username: TextView, password: TextView,passwordlayout : TextInputLayout): Boolean {
+        if(username.text.isEmpty() || password.text.isEmpty()){
+
+            if (password.text.isEmpty()) {
+                password.error = "Password is required"
+                password.requestFocus()
+
+            }
+            // made it revesed so it desplays correctly you ll see it in the app
+            if (username.text.isEmpty()) {
+                username.error = "Username is required"
+                username.requestFocus()
+
+            }
+
+            return false
+
+        }
+        if (username.text.toString() != "admin" && password.text.toString() != "admin") {
+            return false
+        }
+
+        return true
+    }
+
 }

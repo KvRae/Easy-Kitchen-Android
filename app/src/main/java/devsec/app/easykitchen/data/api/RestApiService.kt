e86870eebe98e415edf3391487c424fe00428b29
medyassin-khabthani <mohamedyassine.khabthani@esprit.tn>
2023-01-20 18:02:48 +0100

    search and ingredients filter for user Recipe

--- app/src/main/java/devsec/app/easykitchen/data/api/RestApiService.kt
+++ app/src/main/java/devsec/app/easykitchen/data/api/RestApiService.kt
@@ -138,8 +138,8 @@ interface RestApiService {
 
 class RetrofitInstance {
     companion object {
-//        const val BASE_URL: String = "http://10.0.2.2:3000/api/"
-        const val BASE_URL: String = "http://192.168.1.14:3000/api/"
+        const val BASE_URL: String = "http://10.0.2.2:3000/api/"
+//        const val BASE_URL: String = "http://192.168.1.14:3000/api/"
 
         val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
             this.level = HttpLoggingInterceptor.Level.BODY
